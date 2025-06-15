aa96215b160385e5d26e08f95e4eee537cae27b6
Kimbaro <azxcv1329@gmail.com>
2025-06-15 23:05:52 +0900

    :fix: 병합에 따른 내용 수정

--- MapService/Backend/src/main/java/com/bookfit/www/map/db/repo/SampleRepository.java
+++ MapService/Backend/src/main/java/com/bookfit/www/map/db/repo/SampleRepository.java
@@ -1,15 +1,17 @@
 package com.bookfit.www.map.db.repo;
 
 import com.bookfit.www.map.db.entity.Sample;
+import com.bookfit.www.map.dto.main.MapListStatusCountVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface SampleRepository extends JpaRepository<Sample, Integer>, SampleRepositoryCustom  {
+import java.util.List;
 
-public interface SampleRepository extends JpaRepository<Sample, Integer> {
+public interface SampleRepository extends JpaRepository<Sample, Integer>, SampleRepositoryCustom {
+
 
     @Query(value = "SELECT status AS status, COUNT(*)::INT AS count " +
             "FROM sample WHERE status IN ('PENDING', 'REGISTERED') GROUP BY status",
