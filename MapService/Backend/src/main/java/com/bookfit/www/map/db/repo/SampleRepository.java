82c5329809ddbfc3608ecfc1d1d106fb0d632077
seal5945 <seal5945@gmail.com>
2025-06-15 22:32:53 +0900

    :zep: 등록공간 저장요청승인

--- MapService/Backend/src/main/java/com/bookfit/www/map/db/repo/SampleRepository.java
+++ MapService/Backend/src/main/java/com/bookfit/www/map/db/repo/SampleRepository.java
@@ -1,11 +1,13 @@
 package com.bookfit.www.map.db.repo;
 
 import com.bookfit.www.map.db.entity.Sample;
-import com.bookfit.www.map.dto.main.MapListStatusCountVO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+public interface SampleRepository extends JpaRepository<Sample, Integer>, SampleRepositoryCustom  {
 
 public interface SampleRepository extends JpaRepository<Sample, Integer> {
 
